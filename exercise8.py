b95792ae713f114e277f7ef2f7a548cb4aadb77d
bernzdgzmnps23 <bernzldg555@gmail.com>
2025-06-10 13:11:11 +0800

    Test Upload

---
+++ exercise8.py
@@ -0,0 +1,13 @@
+A = 1
+while A < 30:
+    print(A)
+    if A > 20:
+        break
+    A += 6
+
+myval = 45
+while myval < 90:
+    print (myval)
+    if myval > 80:
+        break
+    myval +=8    
